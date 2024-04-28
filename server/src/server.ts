2daded5631cd2931b539fa31a4e3235d4473f85d
Tiago Santos <tiago881@gmail.com>
2024-04-28 18:01:06 -0300

    feat: Add endpoint to fetch all habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Hello Node js";
+app.get("/habits", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
 });
 
 app
