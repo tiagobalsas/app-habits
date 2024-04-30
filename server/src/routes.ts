ea0f38b8d6fe95911b32e6e7b40c04dede5806aa
Tiago Santos <tiago881@gmail.com>
2024-04-30 18:59:28 -0300

    feat: add POST /habits route to create new habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,30 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { startOfDay } from 'date-fns';
 import { prisma } from '@/lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/habits', async () => {
-    const habits = await prisma.habit.findMany();
+  app.post('/habits', async (request) => {
+    const createHabitbody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = createHabitbody.parse(request.body);
 
-    return habits;
+    const toDay = startOfDay(new Date());
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: toDay,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
   });
 }
