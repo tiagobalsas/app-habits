100aec6c8b0615b1c5912906ea4bc2094974a925
Tiago Santos <tiago881@gmail.com>
2024-04-30 17:09:06 -0300

    refactor: Move '/habits' route to a separate routes file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,12 @@
 import Fastify from 'fastify';
-
 import cors from '@fastify/cors';
-import { prisma } from '@/lib/prisma';
+
+import { appRoutes } from '@/routes';
 
 const app = Fastify();
 
 app.register(cors);
-
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany();
-
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
