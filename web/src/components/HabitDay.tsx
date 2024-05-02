8554bf67bdbaa6c2f250d1f9539e7ec853a8dbac
Tiago Santos <tiago881@gmail.com>
2024-05-02 16:45:55 -0300

    refactor: Rename Habits component to HabitDay and refactor code

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,8 @@
-interface HabitsProps {
-  completed: number;
-}
 
-export function Habits({ completed }: HabitsProps) {
+export function HabitDay() {
   return (
     <div className="bg-zinc-900 w-10 text-white rounded m-2 flex items-center justify-center">
-      {completed}
+      <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
     </div>
   );
 }
