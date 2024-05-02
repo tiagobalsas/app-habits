8d8d07aee8916af68bbae00a983342573118473d
Tiago Santos <tiago881@gmail.com>
2024-05-02 15:57:15 -0300

    feat: Add logo display to app.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
-import '@/styles/global.css'
-import { Habits } from "@/components/Habits";
+import '@/styles/global.css';
+// import { Habits } from '@/components/Habits';
 
-function App() {
+import logoHabits from '@/assets/logo.svg';
+
+export function App() {
   return (
-    <div>
-      <Habits completed={3} />
-      <Habits completed={4} />
-      <Habits completed={5} />
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className=' w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoHabits} alt='Habits' />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
