c29d7585ce4a19602f068eb2edfd77ea80875f13
Tiago Santos <tiago881@gmail.com>
2024-05-06 15:22:54 -0300

    feat: Add HabitDay component and import in Home

---
+++ mobile/src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+import { TouchableOpacity } from 'react-native';
+
+export function HabitDay(){
+  return(
+    <TouchableOpacity
+      className='bg-zinc-900 rounded-lg border-2 border-zinc-800'
+    />
+  )
+}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,13 @@
-import { Header } from '@/components/Header';
 import { View } from 'react-native';
+
+import { HabitDay } from '@/components/HabitDay';
+import { Header } from '@/components/Header';
 
 export function Home() {
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
+      <HabitDay />
     </View>
   );
 }
