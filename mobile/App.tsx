cb3e686cd9af62e0c2efbb5c7fdfed86b818d4de
Tiago Santos <tiago881@gmail.com>
2024-05-06 15:12:35 -0300

    feat: Add Home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StatusBar } from 'react-native';
 import {
   useFonts,
   Inter_400Regular,
@@ -6,9 +6,10 @@ import {
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold,
-} from "@expo-google-fonts/inter";
+} from '@expo-google-fonts/inter';
 
-import { Loading } from "@/components/Loading";
+import { Loading } from '@/components/Loading';
+import { Home } from '@/screens/Home';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -24,28 +25,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <>
+      <Home />
       <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
+        barStyle='light-content'
+        backgroundColor='transparent'
         translucent
       />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#FFF",
-    fontFamily: "Inter_800ExtraBold",
-  },
-});
