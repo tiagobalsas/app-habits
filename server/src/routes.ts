3cb2c66e827cf66eb359a96a83c22cc7dfc0e1ca
Tiago Santos <tiago881@gmail.com>
2024-05-07 15:51:50 -0300

    feat: Implement habit toggle functionality

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
-import { startOfDay, getDay, parseISO } from 'date-fns';
+import { startOfDay, getDay } from 'date-fns';
+
 import { prisma } from '@/lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
@@ -68,5 +69,53 @@ export async function appRoutes(app: FastifyInstance) {
       possibleHabits,
       completedHabits,
     };
+  });
+
+  app.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = startOfDay(new Date());
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
   });
 }
