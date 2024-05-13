d57b6c4be08f6b1a24f0c640c4a99a7cfff6680e
Tiago Santos <tiago881@gmail.com>
2024-05-13 17:03:23 -0300

    feat: create BackButton component and import in NewHabit screen

--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -1,9 +1,11 @@
-import { View } from "react-native";
+import { View } from 'react-native';
+
+import { BackButton } from '@/components/BackButton';
 
 export function NewHabit() {
-  return(
-    <View className="flex-1 bg-background px-8 pt-16">
-      
+  return (
+    <View className='flex-1 bg-background px-8 pt-16'>
+      <BackButton />
     </View>
-  )
+  );
 }
