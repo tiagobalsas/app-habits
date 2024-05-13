716ba7db3041d6646bea142a532ddcfe657dd96c
Tiago Santos <tiago881@gmail.com>
2024-05-13 16:15:48 -0300

    feat: create app routes with react-navigation

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,26 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+import { Home } from '@/screens/Home';
+import { NewHabit } from '@/screens/NewHabit';
+import { Habit } from '@/screens/Habit';
+
+export function appRoutes(){
+  <Navigator>
+    <Screen 
+      name='home'
+      component={Home}
+    />
+
+    <Screen 
+      name='new'
+      component={NewHabit}
+    />
+    
+    <Screen 
+      name='habit'
+      component={Habit}
+    />
+  </Navigator>
+}
