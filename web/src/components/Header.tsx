ca46923d8058dfa018fdd5e8d44c755c77cdd93e
Tiago Santos <tiago881@gmail.com>
2024-05-13 10:15:25 -0300

    feat: add Dialog component from radix-ui

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,19 +1,30 @@
 import { Plus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoHabits from '@/assets/logo.svg';
 
 export function Header() {
   return (
     <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
-      <img src={logoHabits} alt='Habits' />
+      <Dialog.Root>
+        <img src={logoHabits} alt='Habits' />
 
-      <button
-        type='button'
-        className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
-      >
-        <Plus size={20} />
-        Novo hábito
-      </button>
+        <Dialog.Trigger
+          type='button'
+          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+        >
+          <Plus size={20} className='text-violet-500' />
+          Novo hábito
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='w-screen h-screen bg-black/80 fixed inset-0' />
+
+          <Dialog.Content className='absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+            Conteúdo do modal
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
