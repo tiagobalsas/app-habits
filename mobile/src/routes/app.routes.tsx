97beba7b05737b844dff51e23a74beff42d6b9a9
Tiago Santos <tiago881@gmail.com>
2024-05-13 16:47:03 -0300

    types: define RootParamList interface for route parameters

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -11,7 +11,7 @@ export function AppRoutes() {
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name='home' component={Home} />
 
-      <Screen name='new' component={NewHabit} />
+      <Screen name='newHabit' component={NewHabit} />
 
       <Screen name='habit' component={Habit} />
     </Navigator>
