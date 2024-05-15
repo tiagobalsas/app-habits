1494c3ad199109f7c3f588e66bd155f179d18ac8
Tiago Santos <tiago881@gmail.com>
2024-05-15 16:55:10 -0300

    feat: add HabitsInfo state and API call to HabitsList

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -47,7 +47,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <HabitsList />
+          <HabitsList date={date} />
 
           <Popover.Arrow height={8} className='fill-zinc-900' />
         </Popover.Content>
