f5c320b5a6e234bc8f4c5c077c58b8e90eed9096
Tiago Santos <tiago881@gmail.com>
2024-05-15 16:26:52 -0300

    feat: add HabitsList component to HabitDay

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,14 +1,12 @@
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
-import { Check } from 'phosphor-react';
-
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 
 import clsx from 'clsx';
 
 import { ProgressBar } from '@/components/ProgressBar';
+import { HabitsList } from '@/components/HabitsList';
 
 interface HabitDayProps {
   date: Date;
@@ -49,19 +47,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root className='flex items-center gap-3 group'>
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                <Checkbox.Indicator>
-                  <Check size={20} className='text-white' />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList />
 
           <Popover.Arrow height={8} className='fill-zinc-900' />
         </Popover.Content>
