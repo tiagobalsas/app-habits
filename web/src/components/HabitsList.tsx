39ac0898b16e9ca6124b2a1ac180f6c2d0426552
Tiago Santos <tiago881@gmail.com>
2024-05-16 17:33:03 -0300

    style: add focus styles in HabitsList

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -72,7 +72,7 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
             disabled={isDateInPost}
             className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
           >
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
               <Checkbox.Indicator>
                 <Check size={20} className='text-white' />
               </Checkbox.Indicator>
