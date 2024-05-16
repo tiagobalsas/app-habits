edb23d445120dc68e7aa325e5878c57cab93b76a
Tiago Santos <tiago881@gmail.com>
2024-05-16 17:08:14 -0300

    style: add transition-colors to checkbox in NewHabitForm

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -76,7 +76,7 @@ export function NewHabitForm() {
               checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
             >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors'>
                 <Checkbox.Indicator>
                   <Check size={20} className='text-white' />
                 </Checkbox.Indicator>
