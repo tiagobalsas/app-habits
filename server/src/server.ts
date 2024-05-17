1d92813950df27efb613c073742226ad1d803489
Tiago Santos <tiago881@gmail.com>
2024-05-17 15:38:35 -0300

    feat: update server listen configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,7 @@ app.register(appRoutes);
 
 app
   .listen({
-    port: 3333,
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
   })
   .then(() => console.log('HTTP Server is running!'));
